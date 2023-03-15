a5da6839e0c6a147345a54d37cd178c54828cee1
robertel15 <s212463@student.dhbw-mannheim.de>
2023-03-15 15:55:55 +0100

    Grundlagen geschaffen

---
+++ src/main/java/de/LucaR/Fahrradverleih/FahrradverleihApplication.java
@@ -0,0 +1,13 @@
+package de.LucaR.Fahrradverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FahrradverleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FahrradverleihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/LucaR/Fahrradverleih/FahrradverleihApplicationTests.java
@@ -0,0 +1,13 @@
+package de.LucaR.Fahrradverleih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FahrradverleihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
